9f1767c3c801f8f3eaca0497cd603429900d8866
Eerik <eerik.valguta@gmail.com>
2023-10-16 14:36:34 +0300

    16.10

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::latest()->paginate();
+        return view('articles.index', compact('articles'));
+    }
+
+    public function deleted()
+    {
+        $articles = Article::onlyTrashed()->orderby('deleted_at')->paginate();
+        return view('articles.index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArticleRequest $request)
+    {
+
+        $article = new Article($request->validated());
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Article $article)
+    {
+        return view('articles.edit', compact('article'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArticleRequest $request, Article $article)
+    {
+        //    $article->title = $request->validated('title');
+        //    $article->body = $request->validated('title');
+        $article->fill($request->validated());
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+}
